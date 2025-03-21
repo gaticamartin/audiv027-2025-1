95eff0c66f9043aacc2cd27125985b87a925f4d9
Gaboo <94022396+GabooDesign@users.noreply.github.com>
2025-03-21 17:10:13 -0300

    Add files via upload

---
+++ 04-GabooDesign/clase-02/clase_2_WebcamBacan/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  createCapture(VIDEO);
+  describe('Una transmisión de tu Webcam.');
+  
+}
+
+function draw() {
+  
+  ValueX= 200
+  ValueY= 200
+  ellipse(ValueX,ValueY,80,80);
+}
