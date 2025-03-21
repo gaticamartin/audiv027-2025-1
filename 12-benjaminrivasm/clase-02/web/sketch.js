363e16b35b494aa343909dce1a40ee91c709c470
Rivas <benjamin.rivas.m@ug.uchile.cl>
2025-03-21 18:08:17 -0300

    actualizar sketch.js
    cosas que probé en clase

--- 12-benjaminrivasm/clase-02/web/sketch.js
+++ 12-benjaminrivasm/clase-02/web/sketch.js
@@ -1,7 +1,64 @@
+let gridSize = 20;
+let dx = 0;
+let dy = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 700);
+  frameRate(60);
+  cols = width / gridSize;
+  rows = height / gridSize;
+  player = new Player(1, 1)
+  c = new Circle(0, 0, 255, 0, 0)
 }
 
 function draw() {
   background(220);
+  fill(0);
+  player.move(dx, dy);
+  player.show();
+  c.show();
+  c.move();
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) [dx, dy] = [-1, 0];
+  if (keyCode === RIGHT_ARROW) [dx, dy] = [1, 0];
+  if (keyCode === UP_ARROW) [dx, dy] = [0, -1];
+  if (keyCode === DOWN_ARROW) [dx, dy] = [0, 1];
+}
+
+
+class Player{
+  constructor(x, y){
+    this.x = x
+    this.y = y
+  }
+  
+  show (){
+  fill(0)
+  rect(this.x * gridSize, this.y * gridSize, gridSize, gridSize)
+  }
+  
+  move(dx, dy){
+    this.x = (this.x + dx + cols) % cols;
+    this.y = (this.y + dy + rows) % rows;
+  }
+}
+
+class Circle{
+  constructor(x, y, k1, k2, k3){
+    this.x = x
+    this.y = y
+    this.k1 = k1
+    this.k2 = k2
+    this.k3 = k3
+  }
+  show(){
+    fill(this.k1, this.k2, this.k3);
+    ellipse(this.x, this.y, 20, 20);
+  }
+  move(){
+    this.x = (this.x + 20 + width) % width;
+    this.y = (this.y + 20 + height) % height;
+  }
 }
